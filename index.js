31adb7b2daf5fdbbfb0d662a9edd2802ed0b9983
unknown <codyt11@hotmail.com>
2023-11-09 11:47:26 -0600

    added test for api get

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+app.get('/', (req, res) => "app is working!");
+
 app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
